eb50d7f97a579224cb8916a8ce0d10822eb7ca38
rolandoglez <rolandoglez@me.com>
2020-04-27 22:45:40 -0400

    initial commit

---
+++ gatsby-bulma-quickstart/src/components/footer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaTwitter, FaGithub, FaMedium } from 'react-icons/fa'
+import { StaticQuery, graphql } from 'gatsby'
+import './style.scss'
+import Emoji from './emoji'
+
+const Footer = () => (
+  <StaticQuery
+    query={graphql`
+      query SocialQuery {
+        site {
+          siteMetadata {
+            gatsby
+            bulma
+            twitter
+            medium
+            github
+          }
+        }
+      }
+    `}
+    render={data => (
+      <footer className='footer center has-background-light'>
+        <div className='content has-text-centered'>
+          <p className='is-size-12'>
+            using{' '}
+            <a href={data.site.siteMetadata.gatsby}>Gatsby</a> +{' '}
+            <a href={data.site.siteMetadata.bulma}>Bulma</a>
+          </p>
+        </div>
+      </footer>
+    )}
+  />
+)
+
+export default Footer
