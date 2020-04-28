c04ce42876d3ed637b04c253173b18edc910ad8f
rolandoglez <rolandoglez@me.com>
2020-04-28 12:49:33 -0400

    move all files one directory up

---
+++ rolandoglez.github.io/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.scss';
+
+const Navbar = () => (
+	<div className="hero-head is-hidden-mobile">
+		<nav className="navbar">
+		</nav>
+	</div>
+);
+
+export default Navbar;
